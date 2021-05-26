97ed5861bbcfc871d9cdea626b3c3d58e794eeec
gridnev <gridnevmami@gmail.com>
2021-05-26 22:05:49 +0300

    second_commit_first_review_with_JS

--- script/script.js
+++ script/script.js
@@ -1,8 +1,29 @@
-function closePopup() {
-    const popupClose = document.querySelector('.popup_closed');
-    document.removeEventListener('click', closePopup);
-    popupClose.classList.remove('.popup-edit');
+let editButton = document.querySelector('.profile__button-edit');
+let popup = document.querySelector('.popup-edit');
+let closeButton = popup.querySelector('.popup-edit__button-close');
+let submitForm = popup.querySelector('.popup-edit__container');
+let name = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
+let nameInput = popup.querySelector('.popup-edit__name');
+let descriptionInput = popup.querySelector('.popup-edit__description');
+
+function popupOpen() {
+   popup.classList.add('popup-edit_open');
+   nameInput.value = name.textContent;
+   descriptionInput.value = description.textContent;
 }
 
-closePopup ();
+function popupClose() {
+   popup.classList.remove('popup-edit_open');
+}
 
+function formSubmitHandler(evt) {
+   evt.preventDefault();
+   name.textContent = nameInput.value;
+   description.textContent = descriptionInput.value;
+   popupClose();
+}
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+submitForm.addEventListener('submit', formSubmitHandler);
