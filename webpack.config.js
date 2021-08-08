9102646d550e7a6163dcdd5b11914b418f144efa
Vova Gridnev <gridnevmami@gmail.com>
2021-08-09 00:28:35 +0300

    fix: доработка сайта

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf|ico)$/,
         type: 'asset/resource'
       },
       {
